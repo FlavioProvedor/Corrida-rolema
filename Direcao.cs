acef0ed33261158415c51e0f1ee6d7f4920f4128
FlavioProvedor <33044672+FlavioProvedor@users.noreply.github.com>
2017-10-23 22:47:46 -0200

    Add files via upload

---
+++ Direcao.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Direcao : MonoBehaviour {
+	public Text verifica;
+	float horizontal;
+	private float roda;
+	// Use this for initialization
+	void Start () {
+		verifica.text = "";
+		roda = 0.0f;
+		horizontal = 0.0f;
+
+	}
+
+	// FixedUpdate is called once per frame
+	void FixedUpdate ()
+	{
+		verifica.text = horizontal.ToString ();
+
+		if (Input.GetAxis ("Vertical") != 0.0f) {
+			horizontal = Input.GetAxis ("Horizontal");
+			roda += horizontal;
+			transform.Rotate (new Vector3 (0.0f, 0.0f, horizontal) * 90 * Time.deltaTime);
+
+		}
+		else 
+		{
+			if (roda != 0.0f) 
+			{
+				horizontal = -horizontal;
+			
+				transform.Rotate (new Vector3 (0.0f, 0.0f, horizontal) * 150 * Time.deltaTime);
+				roda -= horizontal;
+			}
+		}
+
+
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public int velocidade;
+	public int veloGiro;
+	float horizontal;
+	float vertical;
+	public Text verifica;
+
+
+	// Use this for initialization
+	void Start () {
+		horizontal = 0.0f;
+		vertical = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(Input.GetAxis ("Vertical")!=0.0f){
+			andar ();
+			if (Input.GetAxis ("Horizontal") != 0.0f) {
+				girar ();
+			}
+		}
+
+
+	}
+
+	void girar()
+	{
+		horizontal = Input.GetAxis ("Horizontal");
+		Vector3 giro = new Vector3 (0.0f, horizontal, 0.0f);
+		transform.Rotate (giro * veloGiro * Time.deltaTime);
+	}
+	void andar()
+	{
+		vertical = Input.GetAxis ("Vertical");
+		Vector3 moviment = new Vector3 (0.0f, 0.0f, -vertical);
+		transform.Translate (moviment*velocidade * Time.deltaTime);
+	}
+
+}
