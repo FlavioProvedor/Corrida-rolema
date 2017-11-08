1f01401c5f5e002c2a21270e0f9214408ec7cfc4
FlavioProvedor <33044672+FlavioProvedor@users.noreply.github.com>
2017-11-08 10:18:53 -0200

    Add files via upload
    Scripts Direção para movimentar o pedal do carrinho de rolemã e classe Girar como super classe para PlayerController encapsular dados para Direção.

---
+++ Direcao.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Direcao : MonoBehaviour {
+
+	public GameObject player;
+	private float lado;
+	private float quantoRodou;
+	private Vector3 gira;
+	private Girar virar;
+
+	// Use this for initialization
+	void Start () {
+		setVirar ();
+		lado = virar.getLado ();
+		quantoRodou = virar.getQuantoRodou ();
+
+
+	}
+
+	// FixedUpdate evento em determinado intervalo de tempo
+	void FixedUpdate ()
+	{
+		//Girar o pedal ou retornar a posição inicial
+		if (virar.getRodando ()) {
+			if ((quantoRodou < virar.getLimite ()) && (quantoRodou > -virar.getLimite ())) {
+				girarPedal (lado);
+			} 
+		}else {
+			if (quantoRodou != 0) {
+					girarPedal(-lado);
+				}		
+		}
+	
+
+		
+		setVirar ();
+		quantoRodou = virar.getQuantoRodou ();
+		lado = virar.getLado ();
+
+
+	}//Fim do FixedUpdate
+
+
+	//Carregando as informações de lado e quanto rodou da classe PlayerController
+	public void setVirar()
+	{
+		virar = player.GetComponent<PlayerController>().virar;
+	}
+
+	/**
+	 * Girando o pedal de acordo com o lado
+	 * @param lad	Float que indica o sentido de giro (positivo ou negativo)
+	 */
+	private void girarPedal(float lad)
+	{
+		gira = new Vector3 (0.0f, 0.0f, lad); 
+		transform.Rotate (gira * 90 * Time.deltaTime);
+	}
+
+}
---
+++ Girar.cs
@@ -0,0 +1,69 @@
+﻿/**
+ * Esta classe tem por armazena valores referentes
+ * ao lado que o objeto está rodando e o quanto ja rodou
+*/
+
+public class Girar 
+{
+
+	private float lado;
+	private float quantoRodou;
+	private  float limite;
+	private bool rodando;
+
+	/**
+	 * O construtor inicializa os campos a direção do giro
+	 * e o quanto ja rodou
+	 * @param sentido	lado que o objeto esta rodando
+	 * @param rotacao	O quanto o objeto ja rodou
+	 * @param lim	limite em graus para rotação
+	 **/
+	public Girar(float sentido, float rotacao,float lim)
+	{
+		setLado(sentido);
+		setQuantoRodou(rotacao);
+		setLimite (lim);
+		setRodando (false);
+	}
+	//Construtor sem argumento
+	public Girar()
+	{
+		setLado(0.0f);
+		setQuantoRodou(0.0f);
+		setLimite (0.0f);
+	}
+	//Gets e Sets para lado e quantoRodou
+	public float getQuantoRodou()
+	{
+		return quantoRodou;
+	}
+	public void setQuantoRodou(float rotacao)
+	{
+		quantoRodou = rotacao;
+	}
+	public float getLado()
+	{
+		return lado; 
+	}
+	public void setLado(float sentido)
+	{
+		lado = sentido;
+	}
+	public float getLimite()
+	{
+		return limite; 
+	}
+	public void setLimite(float fim)
+	{
+		limite = fim;
+	}
+
+	public void setRodando(bool valor)
+	{
+		rodando = valor;
+	}
+	public bool getRodando()
+	{
+		return rodando; 
+	}
+}
