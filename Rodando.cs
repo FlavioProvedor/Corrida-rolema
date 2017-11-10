9feaa1d49dcf17094cb279e85155f38dca6f96ab
FlavioProvedor <33044672+FlavioProvedor@users.noreply.github.com>
2017-11-10 10:25:57 -0200

    Add files via upload
    Script para rodar as notas.

---
+++ Rodando.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Este script tem por objetivo fazer as notas rodarem
+public class Rodando : MonoBehaviour {
+
+	// Não terão campos a serem inicializados
+	void Start () {
+		
+	}
+	
+	//Faz as notas girarem
+	void FixedUpdate () {
+		transform.Rotate (0, 4, 0);
+	}
+
+}
